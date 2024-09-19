781dcfefa5db63f03234541b4bac039d8f6430d3
Harsh Sharma <harsh@codelif.in>
2024-09-20 02:11:13 +0530

    docs: gh again

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import pyjiit
 
 project = 'pyjiit'
 copyright = '2024, Harsh Sharma'
